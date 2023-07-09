c3a85de92c78687f2cd6999a8a8aab5fb7d370c6
Alexander Peregontsev <110561969+APeregontsev@users.noreply.github.com>
2023-07-09 06:08:42 +0300

    Delete src directory

--- src/serverAddress.js
+++
@@ -1,5 +0,0 @@
-/* export const serverAddress = "https://cra-test-shop-server.onrender.com/"; */
-
-export const serverAddress = "http://localhost:3100/";
-
-/*  "homepage": "https://aperegontsev.github.io/cra_test_shop_front", */
--- src/ui/loader/index.js
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-import classNames from "classnames";
-
-const Loading = () => {
-  return <div className="loader"></div>;
-};
-
-export default Loading;
